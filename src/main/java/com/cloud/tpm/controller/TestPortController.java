795863dc9140ec45099d3a56a0575450f7119791
TPMThreeperson <35865916+TPMThreeperson@users.noreply.github.com>
2018-02-05 13:12:52 +0700

    Add files via upload

--- src/main/java/com/cloud/tpm/controller/TestPortController.java
+++ src/main/java/com/cloud/tpm/controller/TestPortController.java
@@ -1,11 +1,19 @@
 package com.cloud.tpm.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cloud.tpm.model.Request;
+import com.cloud.tpm.model.Responds;
 import com.mysql.fabric.xmlrpc.base.Data;
+
+
 
 @RestController
 public class TestPortController {
@@ -15,5 +23,12 @@ public class TestPortController {
 		Data date=new Data();
 		String TestService="TPM THhreeperson "+date.toString();
 		return TestService;
+	}
+	@PostMapping(value ="/check")
+	@ResponseBody
+	public Responds TestRequest(@Valid @RequestBody Request request,HttpServletRequest http) {
+		Responds res=new Responds();
+		res.setName(res.getName()+" Responds for postman");
+		return res;
 	}
 }
---
+++ src/main/java/com/cloud/tpm/model/Responds.java
@@ -0,0 +1,14 @@
+package com.cloud.tpm.model;
+
+public class Responds {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
